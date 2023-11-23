d50f931d7aca5b4348ebe84433080cd9dcecf9e3
Austin Vichich <avichich4@gmail.com>
2023-11-22 20:48:07 -0700

    completed lab

--- index.js
+++ index.js
@@ -1,11 +1,3 @@
-//function fetchBooks() {
-//  fetch("https://anapioficeandfire.com/api/books")
-//    .then((resp) => resp.json())
-//    .then((json) => {
-//      renderBooks(json);
-//    });
-//}
-
 function fetchBooks() {
   return fetch("https://anapioficeandfire.com/api/books")
     .then(function(response) {
